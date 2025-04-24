aefd7912339d859f5e4ef8d4bafc6941028193b3
Phil Schwartz <philschwartz@fb.com>
2025-04-24 16:23:11 -0500

    commit 1 & 2

--- packages/hyperion-react-testapp/src/App.tsx
+++ packages/hyperion-react-testapp/src/App.tsx
@@ -112,6 +112,7 @@ function App() {
           {Object.keys(Modes).map(key => <option key={key} value={key}>{key}</option>)}
         </select>
         {Modes[mode]()}
+        <div>Test Phil & Xiaoling</div>
       </div>
     }
 
